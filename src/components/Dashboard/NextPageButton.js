7bfb069fded5267fa9f04ef183599aefbac96517
Nikolay Dimov <nikolay.dimov83@gmail.com>
2024-05-12 10:03:39 +0300

    Fixed the pagination with disabled

--- src/components/Dashboard/NextPageButton.js
+++ src/components/Dashboard/NextPageButton.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import {DashboardContext} from '../../contexts/DashboardContext';
-import styles from './PageButton.module.css'; // Import the CSS module
+import styles from './PageButton.module.css';
 const pageLength=500
 const NextButton = () => {
     const { page, setPage,dashboardState } = useContext(DashboardContext);
@@ -13,7 +13,7 @@ const NextButton = () => {
         
     };
     return (
-        <button /*disabled={page+1<totalPages}*/ onClick={handleClick} className={styles["page"]}> {/* Add the className prop */}
+        <button disabled={page+1>totalPages} onClick={handleClick} className={styles["page"]}> {/* Add the className prop */}
             Next
         </button>
     );
